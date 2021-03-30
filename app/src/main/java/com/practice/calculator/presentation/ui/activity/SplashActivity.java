45ef298d7001d5c70166d912b5b7e6515d761c95
shgrg1712 <shgrg1712@gmail.com>
2021-03-30 09:23:34 +0530

    ADDED :: SplashActivity code

---
+++ app/src/main/java/com/practice/calculator/presentation/ui/activity/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.practice.calculator.presentation.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.practice.calculator.R;
+
+import static com.practice.calculator.presentation.ui.utilities.Constants.SPLASH_SCREEN_TIME_OUT;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
