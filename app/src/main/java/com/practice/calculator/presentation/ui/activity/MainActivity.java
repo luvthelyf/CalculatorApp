e899f9b839ccd995585bc583356d908b1e8e0892
baghira <sandeep.gupta@tokopedia.com>
2021-03-31 09:02:37 +0530

    Added constraintLayout

--- app/src/main/java/com/practice/calculator/presentation/ui/activity/MainActivity.java
+++ app/src/main/java/com/practice/calculator/presentation/ui/activity/MainActivity.java
@@ -1,22 +1,32 @@
 package com.practice.calculator.presentation.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.practice.calculator.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = findViewById(R.id.toolbar);
-//        getActionBar().setHomeButtonEnabled(true);
-//        getActionBar().setTitle("Howwww");
 
+
+    }
+
+    public void digitClick(View view) {
+
+    }
+
+    public void symbolClick(View view) {
+    }
+
+    public void clearClick(View view) {
+    }
+
+    public void equalClick(View view) {
     }
 }
