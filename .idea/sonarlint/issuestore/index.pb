848f2ff1e6e619c0165d29751dd4f84187a600e9
baghira <sandeep.gupta@tokopedia.com>
2021-03-31 08:19:50 +0530

    Added toolbar

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+†
+Vapp/src/main/java/com/practice/calculator/presentation/ui/fragment/EditorFragment.java,e/7/e705296a4489804e37265611615574fe4a2d7128
+„
+Tapp/src/main/java/com/practice/calculator/presentation/ui/activity/MainActivity.java,e/6/e6e2330a0c7e25b36ea29f12bc56bf093ac3fcdc
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
+‡
+Wapp/src/main/java/com/practice/calculator/presentation/ui/fragment/EditorViewModel.java,c/5/c50bcee5a5097326f29a01c8b37c3a6081f91103
--- app/src/main/java/com/practice/calculator/presentation/ui/activity/MainActivity.java
+++ app/src/main/java/com/practice/calculator/presentation/ui/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.practice.calculator.presentation.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -8,9 +9,14 @@ import com.practice.calculator.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+//        getActionBar().setHomeButtonEnabled(true);
+//        getActionBar().setTitle("Howwww");
+
     }
 }
