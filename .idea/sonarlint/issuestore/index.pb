13bc60f2cb5853644a078c7de5e4df2203a486b9
baghira <sandeep.gupta@tokopedia.com>
2021-04-01 08:17:52 +0530

    equation added.

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -11,3 +11,11 @@ P
  app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
 ‡
 Wapp/src/main/java/com/practice/calculator/presentation/ui/fragment/EditorViewModel.java,c/5/c50bcee5a5097326f29a01c8b37c3a6081f91103
+T
+$.idea/codeStyles/codeStyleConfig.xml,d/d/dd7b3350c29c49497756c7f32c69a3bed18b1cba
+L
+.idea/codeStyles/Project.xml,6/e/6ecd6000a7b6f4a2884412ff19f74193ed089648
+R
+"app/src/main/res/values/themes.xml,e/b/eb7f0f821bde169dd21862705b2b042d2cea69a3
+X
+(app/src/main/res/values-night/themes.xml,8/c/8c3a6b96d9ffba496992e612d58227c19e6b9e65
--- app/src/main/java/com/practice/calculator/presentation/ui/activity/MainActivity.java
+++ app/src/main/java/com/practice/calculator/presentation/ui/activity/MainActivity.java
@@ -2,6 +2,9 @@ package com.practice.calculator.presentation.ui.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,24 +12,97 @@ import com.practice.calculator.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
+    Double number1 = 0.0;
+    TextView textView;
+    int lastOperation;
+    boolean isEqualClicked = false;
+    boolean lastNumeric = false;
+    boolean lastDot = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        editText = findViewById(R.id.edt1);
+        textView = findViewById(R.id.equation);
     }
 
     public void digitClick(View view) {
-
+        editText.append(((Button) view).getText());
+        if(isEqualClicked) {
+            isEqualClicked = false;
+            textView.setText("");
+        }
+        lastNumeric = true;
     }
 
     public void symbolClick(View view) {
+
+        if (view.getId() == R.id.buttonadd) {
+            number1 = Double.parseDouble(editText.getText().toString());
+            textView.setText(number1 + " + ");
+            editText.setText("");
+            lastOperation = R.id.buttonadd;
+        } else if (view.getId() == R.id.buttonsub) {
+            number1 = Double.parseDouble(editText.getText().toString());
+            textView.setText(number1 + " - ");
+            editText.setText("");
+            lastOperation = R.id.buttonsub;
+        } else if (view.getId() == R.id.buttonmul) {
+            number1 = Double.parseDouble(editText.getText().toString());
+            textView.setText(number1 + " * ");
+            editText.setText("");
+            lastOperation = R.id.buttonmul;
+        } else if (view.getId() == R.id.buttondiv) {
+            number1 = Double.parseDouble(editText.getText().toString());
+            textView.setText(number1 + " / ");
+            editText.setText("");
+            lastOperation = R.id.buttondiv;
+        }
     }
 
     public void clearClick(View view) {
+        editText.setText("");
+        textView.setText("");
     }
 
     public void equalClick(View view) {
+        isEqualClicked = true;
+        long number2 = Long.parseLong(editText.getText().toString());
+        double result = 0;
+        switch (lastOperation) {
+
+            case R.id.buttonadd:
+                result = (double)number1 + number2;
+                textView.setText(textView.getText().toString() + number2 + " = " + result);
+                break;
+
+            case R.id.buttonsub:
+                result = (double)number1 - number2;
+                textView.setText(textView.getText().toString() + number2 + " = " + result);
+                break;
+            case R.id.buttonmul:
+                result = (double)number1 * number2;
+                textView.setText(textView.getText().toString() + number2 + " = " + result);
+                break;
+            case R.id.buttondiv:
+                try {
+                    result = (double)number1 / number2;
+                    textView.setText(textView.getText().toString() + number2 + " = " + result);
+                } catch (ArithmeticException exception) {
+                    textView.setText("Invalid expression!");
+                }
+                break;
+        }
+        editText.setText("");
+    }
+
+    public void onDecimalPointClick(View view) {
+        if(lastNumeric && !lastDot) {
+            editText.append(".");
+            lastNumeric = false;
+            lastDot = true;
+        }
     }
 }
